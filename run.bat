eac2cae9a0d481a901cecb23c96b55a11cce4dfc
wong kalahan <prabaswara.rian06@gmail.com>
2026-07-09 16:59:57 +0700

    run.bat: force UTF-8 log encoding

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d "%~dp0"
-powershell -Command "python -u run_nav_analysis.py 2>&1 | Tee-Object -FilePath nav_log.txt -Append"
+powershell -Command "python -u run_nav_analysis.py 2>&1 | ForEach-Object { Write-Host $_; Add-Content nav_log.txt $_ -Encoding UTF8 }"
